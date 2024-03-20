7f2f2e809d2de0acf3bd881e2fee6181401c0881
Tim Kay <timkay@not.com>
2024-03-20 12:55:28 -0700

    Update bintree2.py

--- bintree2.py
+++ bintree2.py
@@ -27,11 +27,11 @@ class Node():
         if self.left:
             self.left.tree(depth - 1)
         else:
-            print(' ', '-' * (width - 1), end = '')
+            print(' ', '-' * width, end = '')
         if self.right:
             self.right.tree(depth - 1)
         else:
-            print(' ', '-' * (width - 1), end = '')
+            print(' ', '-' * width, end = '')
     def __str__(self):
         return f'Node({self.value})'
 class BinTree():
