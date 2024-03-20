03c2928e36794512a160d3f36eedd4832d38a3b9
Tim Kay <timkay@not.com>
2024-03-20 12:39:37 -0700

    Update bintree2.py

--- bintree2.py
+++ bintree2.py
@@ -80,7 +80,10 @@ class BinTree():
         if what is not None:
             print(f'{what}:')
         if self.root.right is not None:
-            for depth in range(0, 10):
+            wid = 5
+            max = 6
+            for depth in range(0, max):
+                print(' ' * (wid * (max - depth), end='')
                 self.root.right.tree(depth)
                 print()
         return self
