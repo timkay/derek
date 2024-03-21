9f4fbe68080e65af48a6b8bf4f8be59d95f79211
Tim Kay <timkay@not.com>
2024-03-20 19:35:19 -0700

    Update bintree2.py

--- bintree2.py
+++ bintree2.py
@@ -76,7 +76,7 @@ class BinTree():
             print(f'{what}:')
         if self.root.right is not None:
             for depth in range(0, deepest):
-                between = width * (2^(deepest-1) - 2^depth) / (2^depth + 1)
+                between = int(width * (2^(deepest-1) - 2^depth) / (2^depth + 1))
                 self.root.right.tree(depth, between)
                 print()
         return self
