64c6cb697b4357b86269321d59d3146be635ea29
Tim Kay <timkay@not.com>
2024-03-22 17:13:06 -0700

    Update bintree2.py

--- bintree2.py
+++ bintree2.py
@@ -76,11 +76,11 @@ class BinTree():
             def recurse(node, depth=0, side=None, fill=' '):
                 if depth >= maxdepth: return
                 lfill = rfill = ' '
-                if depth == i - 1:
+                if i == depth + 1:
                     if side == 'l': rfill = '-'
                     if side == 'r': lfill = '-'
                 recurse(node.left or none, depth + 1, 'l', lfill)
-                if depth == i:
+                if i == depth:
                     print(f' {node.value}', end='')
                 else:
                     print(fill * (1 + len(str(node.value))), end='')
