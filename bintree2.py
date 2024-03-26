a62125d528579a459f53ed68d0c550d9fe1d8ff6
Tim Kay <timkay@not.com>
2024-03-25 21:30:09 -0700

    Update bintree2.py

--- bintree2.py
+++ bintree2.py
@@ -92,7 +92,8 @@ def print_tree(node):
             if i == level + 1:
                 wid = len(str(node.value))
                 half = int((wid - 1) / 2)
-            print('A' * half + tee + dash * half + (dash if wid % 2 == 0 else ''), end='')
+                s = dash * half + tee + dash * half + (dash if wid % 2 == 0 else '')
+            print(s, end='')
             recurse(node.right or none, level + 1, 'r', fill)
     depth = get_depth(node)
     for i in range(0, depth):
